89c154c6ff8b929c00c55a35c8a16d0e6d9b63b3
Jakob Persons <jakobep24@gmail.com>
2020-10-16 15:39:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,8 +2,7 @@
 require 'pry'
 
 def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
-  return star_date
+  (rand(100000) + 400000) / 10.0
   binding.pry
   puts "remove this line when done"
 end
