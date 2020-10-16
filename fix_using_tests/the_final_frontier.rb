fa4bcd7f476e0abbb935f2ee503cc4a99d135da7
Jakob Persons <jakobep24@gmail.com>
2020-10-16 15:56:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,17 +5,20 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
+generate_star_date
 star_date = generate_star_date
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+state_log(star_date)
+
 crew = ["Jake", "Brynn", "Chase", "Brittany", "Eric"]
 
 def greet_crew(crew)
   crew.each do |crew_member|
-    "Hello #{crew_member}."
+    puts "Hello #{crew_member}."
   end
 end
 
