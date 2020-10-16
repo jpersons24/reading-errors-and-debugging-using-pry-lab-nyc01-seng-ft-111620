dd7241c3eb312476322ec985987da3502bec833e
Jakob Persons <jakobep24@gmail.com>
2020-10-16 15:52:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,9 +5,6 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-star_date = generate_star_date
-
-
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
@@ -19,8 +16,6 @@ def greet_crew(crew)
     "Hello #{crew_member}."
   end
 end
-
-state_log(star_date)
 
 def engage
   date = generate_star_date
