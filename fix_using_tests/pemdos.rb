1293f1871e78e24fde1c3da8f90512ab8f0f7924
Jakob Persons <jakobep24@gmail.com>
2020-10-16 14:27:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    (10.times puts "s") + string
+    binding.pry
   else
     string
   end
 end
+
+snake_it_up("surprise")
