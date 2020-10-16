07b88c325471b900586b931858ffa8a725777b79
Jakob Persons <jakobep24@gmail.com>
2020-10-16 16:01:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,7 +5,7 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-generate_star_date #call generate_star_date method 
+generate_star_date #call generate_star_date method
 star_date = generate_star_date #set generate_star_date method equal to star_date variable
 
 def state_log(star_date) #use star_date as argument within state_log method
@@ -15,7 +15,7 @@ end
 crew = ["Jake", "Brynn", "Chase", "Brittany", "Eric"] #set crew equal to array of names
 
 def greet_crew(crew) #us array of names as argument for greet_crew method
-  crew.each do |crew_member| #iterate over crew array and 
+  crew.each do |crew_member| #iterate over crew array and
     puts "Hello #{crew_member}." #puts greeting to each crew member
   end
 end
