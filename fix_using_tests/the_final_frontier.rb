9385bd04fd1b4206d978a44da1a17794f79f9277
Jakob Persons <jakobep24@gmail.com>
2020-10-16 15:49:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,8 +3,6 @@ require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
-  # binding.pry
-  # puts "remove this line when done"
 end
 
 star_date = generate_star_date
@@ -12,16 +10,14 @@ star_date = generate_star_date
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
-  # binding.pry
 end
 
 state_log(star_date)
 
 def engage
-  puts state_log(star_date)
-  binding.pry
   date = generate_star_date
-  binding.pry
+  state_log(date)
+  greet_crew(crew)
 end
 
 engage
