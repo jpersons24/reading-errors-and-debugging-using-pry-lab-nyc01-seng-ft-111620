0369df62b3ff3e481a41b6dae01c4c14f93ced3e
Jakob Persons <jakobep24@gmail.com>
2020-10-16 15:12:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    "s" * 10 + string
+    "s" * 10 + string #"s" is multiplied 10 times before adding the string argument --> pemdos
     # binding.pry
   else
     string
